34d586d4f80e225ea2205bd06919735b93c1af5e
xycong <xycong@uwaterloo.ca>
2016-04-08 02:44:59 -0400

    Added additional functionality

--- server.js
+++ server.js
@@ -21,16 +21,71 @@ app.get('/', function(req, res) {
     res.end('Server Hosted on Heroku\n');
 });
 
-io.on('connection', function(socket) {
-    console.log('a user connected');
+var numUsers = 0;
+var users = [];
+var rooms = [];
 
-    socket.on('disconnect', function() {
-        console.log('user disconnected');
+io.sockets.on('connection', function(socket) {
+    var addedUser = false;
+
+    socket.on('add user', function(username) {
+        if (addedUser) return;
+
+        socket.username = username;
+        users.push(username);
+        console.log(users);
+        ++numUsers;
+        addedUser = true;
     });
 
-    socket.on('message', function(msg) {
-        console.log('message: ' + msg);
-        io.emit('message', msg);
+    socket.on('subscribe', function(room) {
+        console.log('subscribe');
+        socket.join(room);
+        if (rooms.indexOf(room) === -1) {
+            rooms.push(room);
+        }
+        console.log(rooms);
+        // Todo - get history
+        socket.broadcast.to(room).emit('updatechat', 'user has joined room');
+    });
+
+    socket.on('unsubscribe', function(room) {
+        console.log('unsubscribe');
+        socket.leave(room);
+        if (io.sockets.adapter.rooms[room].length === 0) {
+            var index = rooms.indexOf(room);
+            rooms.splice(index, 1);
+        }
+        console.log(rooms);
+        socket.broadcast.to(room).emit('updatechat', 'user has left room');
+    });
+
+    socket.on('message', function(data) {
+        console.log('message');
+        io.in(data.room).emit('message', {
+            username: socket.username,
+            message: data.message
+        });
+    });
+
+    socket.on('typing', function() {
+        io.in(data.room).emit('isTyping', { username: socket.username });
+    });
+
+    socket.on('stop typing', function() {
+        socket.broadcast.emit('stop typing', {
+            username: socket.username
+        });
+    });
+
+    socket.on('disconnect', function() {
+        console.log('disconnect');
+        if (addedUser) {
+            --numUsers;
+            var index = users.indexOf(socket.username);
+            users.splice(index, 1);
+            console.log(users);
+        }
     });
 });
 
